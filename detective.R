474a27ba15c32aba90a6721379384ffc2cbead93
juschu321 <juliaschuhmann@gmx.de>
2019-08-20 11:50:48 +0200

    try to get amount of packages

--- detective.R
+++ detective.R
@@ -1,5 +1,5 @@
-merged_data <-
-  inner_join(tutti_time_monthly_package,
+#the following script serves to check the data (and served to find an inconsistency in the data)
+merged_data <- inner_join(tutti_time_monthly_package,
              packages_per_psy_sub,
              by = c("package" = "package")) %>%
   filter (category == "Item Response Theory (IRT)") %>%
@@ -16,8 +16,7 @@ psychometrics <- tutti_time_monthly_ctv %>%
 
 #ctv plot mit tutti_time_monthly_package
 
-c <-
-  inner_join (tutti_time_monthly_package,
+c <-inner_join (tutti_time_monthly_package,
               packages_per_ctv,
               by = c("package" = "package"))
 
@@ -33,14 +32,21 @@ ctv_date <- ctv_filter %>%
 count_psych_ctv <- ctv_date %>%
   summarise(total = sum(total))
 
-
-
 count_pkg <- packages_per_ctv %>%
   select(ctv, package) %>%
   filter(ctv == "Psychometrics") %>%
   group_by(ctv) %>%
   summarise(count = n_distinct(package))
 
-install.packages("visNetwork")
+#how many packages do we get from download_data_from_CRAN? (workspace from 2019-08-11)
+#result: 3314 entries with 2505 counts (daily over the last years)
+#packages from CTVs because "packages" has been created by packages_per_ctv
+amount_of_packages <- download_statistics%>%
+  count(package)
 
-vignette("Introduction-to-visNetwork") 
+packages_CRAN_1month <- cran_downloads(packages = NULL, from = "2019-07-01", to = "2019-08-01")
+
+top_CRAN_1month <- cran_top_downloads(when = c("last-month"), count = 10)
+
+tutti <- available.packages(filters = "CRAN")
+# 14,782 entries, where I take the date from?
