8b05a1527a393092553a1d455e027a30be40f46c
juschu321 <juliaschuhmann@gmx.de>
2019-08-26 18:08:39 +0200

    computing for thesis

--- ranking_ctv_pkg.R
+++ ranking_ctv_pkg.R
@@ -1,7 +1,24 @@
+dep <- edgelist%>%
+  filter(type %in% c("imports", "depends"))%>%
+  group_by(to)%>%
+  summarise(count = n()) 
+
+
+#ranking over time 
 overview <- tutti_time_monthly_ctv%>%
-  select(ctv, total)%>%
+  filter(month >= as.Date("2012-10-01"), month <= as.Date("2019-07-01"))%>%
+  group_by(ctv,month)%>%
+  summarise(count = sum(total))%>%
+  group_by(month)%>%
+  mutate(rank = 41-rank(interaction(ctv, count)))
+
+overview%>%
   group_by(ctv)%>%
-  summarise(count = sum(total))
+  summarise(mean(rank), sd(rank))
+
+#plot ranking = desaster
+p <- ggplot(overview)+
+  geom_line(aes(month, rank, color = overview$ctv))
 
 
 
@@ -21,6 +38,40 @@ total_psy_pkg_2018_19 <- psy_pkg_monthly%>%
   group_by(package)%>%
   summarise(count = sum(total))
 
+#delete packages which are in more than one CTV 
+total_psy_pkg_2018_19 <- psy_pkg_monthly%>%
+select(package, total, month)%>%
+  filter(month >= as.Date("2018-07-01"), month <= as.Date("2019-07-01"))%>%
+  group_by(package)%>%
+  summarise(count = sum(total))
+
+#psy packages which are in more than one CTV 
+psy_more <- inner_join(more_than_one, packages_per_ctv, by = c("package" = "package") )%>%
+  filter(core == FALSE)%>%
+  filter(ctv == "Psychometrics")
+
+#these psycho packages are in more than one CTV
+psycho_more <- psy_more %>%
+  select(package)
+
+#option to check for packages which are not included in more ctvs 
+n_ctv_p_pkg <- packages_per_ctv %>%
+  filter(core == FALSE) %>%
+  select(package, ctv) %>%
+  group_by(package)%>%
+  count()
+
+tutti_n_p_ctv <- inner_join(tutti_time_monthly_package, n_ctv_p_pkg, by = c("package" = "package"))
+
+not_incl_more_ctvs_psy <- full_join(tutti_n_p_ctv, packages_per_ctv, by = c("package" = "package"))%>%
+  select(package, month, total, n, ctv)%>%
+  filter(ctv == "Psychometrics")%>%
+  filter(n == 1)%>%
+  group_by(package)%>%
+  filter(month >= as.Date("2018-07-01"), month <= as.Date("2019-07-01"))%>%
+  summarise(count = sum(total))
+
+#to get an overview of the role of the categories 
 psy_ov_sub <- inner_join(total_psy_pkg_2018_19, psy_sub, by = c("package" = "package"))
   
 #to figure out which categories of Psychometrics played an important role
@@ -29,8 +80,6 @@ psycho_sub <- inner_join(tutti_time_monthly_package, psy_sub, by = c("package" =
   select(category, month, total)%>%
   filter(month >= as.Date("2013-07-01"), month <= as.Date("2014-07-01"))%>%
   summarise(count = sum(total))
-
-
 
 
 #figure out popular packages of Environmetrics
@@ -43,3 +92,41 @@ merge <- inner_join(tutti_time_monthly_package, packages_per_ctv, by = c("packag
   summarise(count = sum (total))
 
     
+#check the downloads per package during the last year
+invest <- tutti_time_monthly_package%>%
+  filter(month >= as.Date("2018-07-01"), month <= as.Date("2019-07-01"))%>%
+  group_by(package)%>%
+  summarise(count = sum(total))
+
+#compute the variation in the download statistics across the last year (tutti with more CTVs)
+#mean = 105215
+#var = 297339128869
+#sd = 545288
+#min = 182
+#max = 10869025
+invest%>%  
+  summarise(max(count))
+
+#compute the variation in the download statistics across the last year (psycho with more CTVs)
+#mean = 76810
+#var = 81019579721
+#sd = 284639
+#min = 401
+#max = 2712386
+total_psy_pkg_2018_19%>%
+  summarise(max(count))
+
+##compute the variation in the download statisstics across the last year (psycho no more CTVs)
+#mean = 19642
+#var =  6059379666
+#sd =  77842
+#min = 966
+#max = 937852
+not_incl_more_ctvs_psy%>%
+  summarise(mean(count))
+
+#descriptive statistics psycho packages variation (FALSCH)
+Psychometrics <- c(105215,297339128869, 545288, 182,10869025  )
+Psychometrics_without_overlapping_pkg <- c(47435,25349372127, 159215, 1037, 1860598 )
+Z <- rbind(Psychometrics, Psychometrics_without_overlapping_pkg)
+colnames(Z) <- c("mean", "var", "sd", "min", "max")
